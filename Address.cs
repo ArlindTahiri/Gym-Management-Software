eb6b1bd2613ba194a77bdac0469fbb9208379884
Arlind Tahiri <arlind.tahiri@stud.th-rosenheim.de>
2022-11-25 22:28:15 +0100

    Added .gitignore and created the class Address

---
+++ Address.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Address
+{
+    public string street { get; private set; }
+    public int postalCode { get; private set; }
+    public string city { get; private set; }
+    public string country { get; private set; }
+
+    public Addresse(string street, int postalCode, string city, string country)
+	{
+        street = street;
+        postalCode = postalCode;
+        city = city;
+        country = country;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Address address &&
+               street == address.street &&
+               postalCode == address.postalCode &&
+               city == address.city &&
+               country == address.country;
+    }
+}
