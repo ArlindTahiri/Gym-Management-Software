8f775344886312044a04b88d471d05f9fd7608b3
Arlind Tahiri <arlind.tahiri@stud.th-rosenheim.de>
2022-11-25 23:33:50 +0100

    Added solution, project and namespace. But also fixed typos

---
+++ loremipsum/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace loremipsum
+{
+    public class Address
+    {
+        public string Street { get; private set; }
+        public int PostalCode { get; private set; }
+        public string City { get; private set; }
+        public string Country { get; private set; }
+
+        public Address(string street, int postalCode, string city, string country)
+	    {
+            Street = street;
+            PostalCode = postalCode;
+            City = city;
+            Country = country;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Address address &&
+                   Street == address.Street &&
+                   PostalCode == address.PostalCode &&
+                   City == address.City &&
+                   Country == address.Country;
+        }
+    }
+
+}
