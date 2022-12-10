f48703721ca2ef53bcd8ca4ee7faf2cf3d5290ac
Arlind Tahiri <arlind.tahiri@stud.th-rosenheim.de>
2022-12-10 10:45:14 +0100

    Corrected to one Context Class.

--- loremipsum/Persistence/GymContext.cs
+++ loremipsum/Persistence/GymContext.cs
@@ -3,13 +3,17 @@ using loremipsum.Entities;
 
 namespace loremipsum.Persistence
 {
-    public class MemberContext: DbContext
+    public class GymContext: DbContext
     {
         public virtual DbSet<Member> Members { get; set; }
 
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        public virtual DbSet<Contract> Contracts { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database = Members;Trusted_Connection = True;");
+            optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database = Gym;Trusted_Connection = True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -19,6 +23,12 @@ namespace loremipsum.Persistence
                 .HasOne<Contract>(a => a.Contract)
                 .WithMany(b => b.Members)
                 .HasForeignKey(a => a.MemberID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+                modelBuilder.Entity<Contract>()
+                .HasOne<Employee>(a => a.Employee)
+                .WithMany(b => b.Contracts)
+                .HasForeignKey(a => a.EmployeeID)
                 .OnDelete(DeleteBehavior.Cascade);
             */
 
