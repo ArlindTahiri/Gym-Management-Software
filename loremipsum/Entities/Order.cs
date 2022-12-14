7bf457775292554db0d788704500cd84ff9f57a4
studschido2880 <dominik.schiffer@stud.th-rosenheim.de>
2022-12-14 15:24:17 +0100

    Corrected the Order class

--- loremipsum/Entities/Order.cs
+++ loremipsum/Entities/Order.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace loremipsum.Entities
 {
-    internal class Order
+    public class Order
     {
+        public int Price { get; set; }
+
+        public int OrderID { get; }
+
+        public Order(Article article, int amount)
+        {
+
+            OrderID = ++OrderID;
+            Price = article.Price;
+
+            if (amount <= article.ActualStock)
+            {
+                article.ActualStock = article.ActualStock - amount;
+                //SaveChanges()
+            }
+
+        }
     }
 }
