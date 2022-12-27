47e98bc072c91866bb4748a8ee1115f75fe6d413
studschido2880 <dominik.schiffer@stud.th-rosenheim.de>
2022-12-27 18:01:31 +0100

    Started creating GUI together with Lorenz and Arlind

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using loremipsum.Gym;
+using loremipsum.Gym.Persistence;
+using System.Windows;
+using System.Windows.Navigation;
+
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        private readonly IProductAdmin admin;
+        private readonly IProductModule query;
+        public MainWindow()
+        {
+            InitializeComponent();
+            IGymPersistence persistence = new GymPersistenceEF();
+            GymFactory factory = new GymFactory(persistence);
+            admin = factory.GetProductAdmin();
+            query = factory.GetProductModule();
+            Application.Current.Properties.Add("IProductAdmin", admin);
+            Application.Current.Properties.Add("IProductModule", query);
+        }
+    }
+}
