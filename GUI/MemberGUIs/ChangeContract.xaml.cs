5fd1867b3a45cc7d88c5b41539dba2033e240924
Lorenz Huber <huber.lorenz@stud.th-rosenheim.de>
2023-01-11 12:35:12 +0100

    Added the GUI ChangeContract where you can change the contract of an member

--- GUI/MemberGUIs/ChangeContract.xaml.cs
+++ GUI/MemberGUIs/ChangeContract.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using loremipsum.Gym;
+using loremipsum.Gym.Entities;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +17,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Contract = loremipsum.Gym.Entities.Contract;
 
 namespace GUI.MemberGUIs
 {
@@ -20,9 +26,66 @@ namespace GUI.MemberGUIs
     /// </summary>
     public partial class ChangeContract : Page
     {
+
+        private readonly IProductAdmin admin = (IProductAdmin)Application.Current.Properties["IProductAdmin"];
+
+
         public ChangeContract()
         {
             InitializeComponent();
+        }
+
+        private void ChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            int t;
+            Contract c = (Contract)ContractCB.SelectedItem;
+            if (!ContractCB.Text.IsNullOrEmpty())
+            {
+                IList<Member> members = admin.ListMembers();
+                foreach(Member member in members)
+                {
+                    if(member.MemberID == Int32.Parse(IDCheck.Text))
+                    {
+                        if (c.ContractID != member.ContractID)
+                        {
+
+                            admin.UpdateContractFromMember(Int32.Parse(IDCheck.Text), c.ContractID);
+
+                            GymHomepage home = new GymHomepage();
+                            NavigationService.Navigate(home);
+                        }
+                        else
+                        {
+                            WarningText.Text = "Dies ist schon der aktuelle Vertrag des Mitgliedes";
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        WarningText.Text = "Dieses Mitglied existiert nicht";
+                    }
+                }
+            }
+            else
+            {
+                WarningText.Text = "Bitte geben Sie für alle Daten etwas ein!";
+            }
+        }
+
+        private void MemberData_Loaded(object sender, RoutedEventArgs e)
+        {
+            MemberData.ItemsSource = admin.ListMembers();
+        }
+
+        private void IDCheck_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            TextValidation.CheckIsNumeric(e);
+        }
+
+        private void ContractCB_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContractCB.Items.Clear();
+            ContractCB.ItemsSource = admin.ListContracts();
         }
     }
 }
