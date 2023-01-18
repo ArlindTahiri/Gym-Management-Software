e749d5059758a5ac61b292d68474e3a3985e0c1e
Lorenz Huber <huber.lorenz@stud.th-rosenheim.de>
2023-01-18 23:14:59 +0100

    completed UnitTestLogIns and reworked UnitTestMember, so most tests work now

--- UnitTestLoremIpsum/UnitTestLogIn.cs
+++ UnitTestLoremIpsum/UnitTestLogIn.cs
@@ -30,10 +30,9 @@ namespace UnitTestLoremIpsum
 
         public void GenerateTestData()
         {
-            Admin.DeleteLogIns();
             l1 = new LogIn("admin_test", "Passwort+1!", 1);
-            l2 = new LogIn("admin_test", "Passwort+1!", 1);
-            l3 = new LogIn("mitarbeiter_test", "Passwort+1!", 2);
+            l2 = new LogIn("mitarbeiter_test", "Passwort+1!", 2);
+            l3 = new LogIn("admin1_test", "Passwort+1!", 1);
         }
         [TestMethod]
         public void TestLogIn()
@@ -44,40 +43,43 @@ namespace UnitTestLoremIpsum
 
             //Test if l1 is in the database
             Assert.IsTrue(Query.GetLogInDetails(l1.LogInName).CompareTo(l1) == 0);
+            Assert.IsNotNull(Query.GetLogInDetails(l1.LogInName));
 
-            //add an login l2 which is identical with l1
-            Admin.AddLogIn(l2);
-            LogIn newLogIn = Query.GetLogInDetails(l2.LogInName);
-            Assert.IsNull(newLogIn);
-
-            //check if there are LogIns in IList
+            //check if there are logIns in the IList
             IList<LogIn> logIns = Admin.ListLogIns();
             Assert.IsTrue(logIns.Count > 0);
 
-            //add and update login l3
-            Admin.UpdateLogIn(l3.LogInName, "mitarbeiter_test", "passwort+2!", 2);
-            
-            //check if login l3 updated in database
-            newLogIn = Query.GetLogInDetails(l3.LogInName);
-            Assert.IsNull(newLogIn);
-            Assert.IsTrue(newLogIn.LogInName.Equals("mitarbeiter2_test") && newLogIn.LogInPassword.Equals("passwort+2!") && newLogIn.Rank == 2);
+            //add and update login l2
+            Admin.AddLogIn(l2);
+            Admin.UpdateLogIn(l2.LogInName, "mitarbeiter_test", "passwort+2!", 2);
 
-            //update login l3, but the newLogInName is the same
-            Admin.UpdateLogIn(l3.LogInName, l3.LogInName, "passwort+!3", 2);
-            newLogIn = Query.GetLogInDetails(l3.LogInName);
-            Assert.IsTrue(newLogIn.LogInName.Equals("mitarbeiter2_test") && newLogIn.LogInPassword.Equals("passwort+3!") && newLogIn.Rank == 2);
+            //check if login l2 updated in database
+            LogIn newLogIn = Query.GetLogInDetails(l2.LogInName);
+            Assert.IsNotNull(Query.GetLogInDetails(l2.LogInName));
+            Assert.IsTrue(newLogIn.LogInName.Equals("mitarbeiter_test") && newLogIn.LogInPassword.Equals("passwort+2!") && newLogIn.Rank == 2);
 
             //change an login which has an rank 1
-            Admin.UpdateLogIn(l1.LogInName, "admin2_test", "passwort+2!", 1);
+            Admin.UpdateLogIn(l1.LogInName, "admin_test", "passwort+2!", 1);
             newLogIn = Query.GetLogInDetails(l1.LogInName);
-            Assert.IsTrue(newLogIn.LogInName.Equals("admin2_test") && newLogIn.LogInPassword.Equals("passwort+2!") && newLogIn.Rank == 1);
+            Assert.IsTrue(newLogIn.LogInName.Equals("admin_test") && newLogIn.LogInPassword.Equals("passwort+2!") && newLogIn.Rank == 1);
 
-            //change the only login which has an rank 1 to rank != 1
-            Admin.UpdateLogIn(l1.LogInName, "admin3_test", "passwort+3!", 2);
-            newLogIn = Query.GetLogInDetails(l1.LogInName);
-            Assert.IsFalse(newLogIn.LogInName.Equals("admin3_test") && newLogIn.LogInPassword.Equals("passwort+3!") && newLogIn.Rank == 2);
+            //add an second login with rank = 1 and try to change one of them to rank 2
+            Admin.AddLogIn(l3);
+            Admin.UpdateLogIn(l3.LogInName, "admin1_test", "passwort+2!", 2);
+            newLogIn = Query.GetLogInDetails(l3.LogInName);
+            Assert.IsTrue(newLogIn.LogInName.Equals("admin1_test") && newLogIn.LogInPassword.Equals("passwort+2!") && newLogIn.Rank == 2);
 
+            //delete login l2
+            Admin.DeleteLogIn(l2.LogInName);
+            Assert.IsNull(Query.GetLogInDetails(l2.LogInName));
 
+            //change rank from l3 to 1 and than delete l3 
+            Admin.UpdateLogIn(l3.LogInName, l3.LogInName, l3.LogInPassword, 1);
+            Admin.DeleteLogIn(l3.LogInName);
+            Assert.IsNull(Query.GetLogInDetails(l3.LogInName));
+
+            //delete l1 (it is only possible because there is still in db a LogIn with rank == 1
+            Admin.DeleteLogIn(l1.LogInName);
         }
     }
 }
--- UnitTestLoremIpsum/UnitTestMember.cs
+++ UnitTestLoremIpsum/UnitTestMember.cs
@@ -16,9 +16,8 @@ namespace UnitTestLoremIpsum
     {
         public IProductAdmin Admin;
         public IProductModule Query;
-        public Member m1, m2, m3, m4, m5;
+        public Member m1, m2, m3;
         public Contract c1, c2;
-        public Article a1;
 
         [TestInitialize()]
         public void SetUp()
@@ -32,135 +31,64 @@ namespace UnitTestLoremIpsum
 
         public void GenerateTestData()
         {
+            Admin.DeleteMembers();
+            Admin.DeleteContracts();
+
             //add contract so we can add members
-            c1 = new Contract("Premium Plan", 20);
+            c1 = new Contract("Premium Plan_tess", 20);
+            c2 = new Contract("Standart_test", 10);
+        }
+
+        [TestMethod]
+        public void TestMember()
+        {
+            //Add contract to add member m1
             Admin.AddContract(c1);
-            c2 = new Contract("Standart", 10);
-
-            
-
-            //add member so we don't have to add members in every TestMethod
-            m1 = Admin.AddMember(c1.ContractID, "Martin", "Meyer", "Mohrenstrasse 54", 04161, "Leipzig", "Deutschland",
+            m1 = Admin.AddMember(c1.ContractID, "Martin_test", "Meyer", "Mohrenstrasse 54", 04161, "Leipzig", "Deutschland",
                     "martinmeyer@gmail.com", "DE94500105172327561324", new DateTime(1990, 11, 24));
-            m4 = Admin.AddMember(c1.ContractID, "Jan", "Wechlser", "Alt-Moabbit 49", 04541, "Borna", "Deutschland", 
-                    "janwechlser@yourrapide.com", "DE09500105174887754664", new DateTime(1996, 5, 14));
-            m5 = Admin.AddMember(c1.ContractID, "Patrick", "Huber", "Brandenburgische Str 92", 55592, "Desloch", "Deutschland", 
-                    "patrickhuber@rhyta.com", "DE22500105179467727673", new DateTime(1997, 7, 4));
 
-            //add article so we can add orders
-            a1 = new Article("Milk Shake", 3, 20, 20);
-        }
+            //Test if the member is in the database
+            Assert.IsTrue(m1.ContractID == c1.ContractID && m1.Forename.Equals("Martin_test") && m1.Surname.Equals("Meyer") && m1.Street.Equals("Mohrenstrasse 54") &&
+                    m1.PostcalCode == 04161 && m1.City.Equals("Leipzig") && m1.Country.Equals("Deutschland") &&
+                    m1.EMail.Equals("martinmeyer@gmail.com") && m1.Iban.Equals("DE94500105172327561324") && m1.Birthday.Equals(new DateTime(1990, 11, 24)));
 
-        [TestMethod]
-        public void AddMember()
-        {
-            //test if there is an contract
-            Assert.IsTrue(c1.ContractType.Equals("Premium Plan") && c1.Price == 20);
-
-            //Add member m2 and test if m2 is in the database
-            m2 = Admin.AddMember(c1.ContractID, "Jennifer", "Meier", "Rhinstrasse 96", 80711, "München", "Deutschland",
-                  "jennifer.meier@gmail.com", "DE90500105174767217514", new DateTime(1991, 11, 24));
-            Assert.IsTrue(m2.ContractID == c1.ContractID && m2.Forename.Equals("Jennifer") && m2.Surname.Equals("Meier") && m2.Street.Equals("Rhinstrasse 96") &&
-                    m2.PostcalCode == 80711 && m2.City.Equals("München") && m2.Country.Equals("Deutschland") &&
-                    m2.EMail.Equals("jennifer.meier@gmail.com") && m2.Iban.Equals("DE90500105174767217514") && m2.Birthday.Equals(new DateTime(1991, 11, 24)));
-
-            Console.WriteLine("Mitglied erfolgreich hinzugefügt");
-
-            int invalid = -1;
-
-
-            //Add member m3 who has no contract and test if m3 is in the database
-            m3 = Admin.AddMember(invalid, "Anton", "Müller", "Rhinstrasse 90", 80711, "München", "Deutschland",
+            //Add an member m2 with no contract
+            try
+            {
+                m2 = Admin.AddMember(c2.ContractID, "Anton_test", "Müller", "Rhinstrasse 90", 80711, "München", "Deutschland",
                    "anton.mueller@gmail.com", "DE90500105174767217514", new DateTime(1991, 1, 24));
-            Assert.IsNull(m3);
+                Assert.Fail();
+            }
+            catch
+            {
+                Console.WriteLine("Exception erkannt");
+            }
 
-        }
+            //Add an member m3 which is identical with m1
+            try
+            {
+                m3 = Admin.AddMember(c1.ContractID, "Martin_test", "Meyer", "Mohrenstrasse 54", 04161, "Leipzig", "Deutschland",
+                    "martinmeyer@gmail.com", "DE94500105172327561324", new DateTime(1990, 11, 24));
+                Assert.Fail();
+            }
+            catch
+            {
+                Console.WriteLine("Exception erkannt");
+            }
 
-        [TestMethod]
-        public void UpdateMember()
-        {
-            //Update member m1 and test if it has changed
-            Admin.UpdateMember(m1.MemberID, "Swen", "Weber", "Grolmanstraße 94", 28217, "Bremen Steffensweg", "Deutschland",
-                    "swenweber@teleworm.us", "DE94500105171116564145", new DateTime(2000, 2, 2));
-            Assert.IsTrue(m1.Forename.Equals("Swen") && m1.Surname.Equals("Weber") && m1.Street.Equals("Grolmanstraße 94") &&
-                    m1.PostcalCode == 28217 && m1.City.Equals("Bremen Steffensweg") && m1.Country.Equals("Deutschland") &&
-                    m1.EMail.Equals("swenweber@teleworm.us") && m1.Iban.Equals("DE94500105171116564145") && m1.Birthday.Equals(new DateTime(2000, 2, 2)));
+            //check if there are members in the IList
+            IList<Member> members = Admin.ListMembers();
+            Assert.IsTrue(members.Count > 0);
 
-            //Update invalid member //how to test if it did something?
-            int invalid = -1;
-            Admin.UpdateMember(invalid, "Christina", "Eisenhauer", "Grolmanstraße 90", 28210, "Bremen", "Deutschland",
-                    "christinaeisenhauer@teleworm.us", "DE25500105174647679764", new DateTime(2000, 3, 3));
-        }
-
-        [TestMethod]
-        public void UpdateContractFromMember()
-        {
-            //Test if you can update the contract from m1 to c2
-            Admin.UpdateContractFromMember(m1.MemberID, c2.ContractID);
-            Assert.IsTrue(m1.ContractID == c2.ContractID);
-
-            //Test if you can update the contract if (member == null || contract == null)
-            int invalid = -1;
-            Admin.UpdateContractFromMember(invalid, c2.ContractID);
-
-            Admin.UpdateContractFromMember(m1.MemberID, invalid);
-            Assert.IsTrue(m1.ContractID != invalid);
-
-            Admin.UpdateContractFromMember(invalid, invalid);
-
-            //Test if contract can't be checkouted-> contract should not change            
-            Admin.UpdateContractFromMember(m1.MemberID, c1.ContractID);
-            Assert.IsTrue(m1.ContractID != c1.ContractID);
-        }
-
-        [TestMethod]
-        public void ListMembers()
-        {
-            //Test should not be null because we added some members
-            Assert.IsNotNull(Admin.ListMembers());
-        }
-
-        [TestMethod]
-        public void DeleteMember()
-        {
-            //Test if we can delete an member
+            //test if we can delete m1 who is currently training
+            Admin.InsertTrainingMember(m1.MemberID);
             Admin.DeleteMember(m1.MemberID);
-            Assert.IsNull(m1);
+            Assert.IsNotNull(Query.GetMemberDetails(m1.MemberID));
+            Admin.DeleteTrainingMember(m1.MemberID);
 
-            //Test if we can delete m4 who is currently training
-            Admin.InsertTrainingMember(m4.MemberID);
-            Admin.DeleteMember(m4.MemberID);
-            Assert.IsNotNull(m4);
-            Admin.DeleteTrainingMember(m4.MemberID);
-
-            //Test if we can delete an invalid member
-            int invalid = -1;
-            Admin.DeleteMember(invalid);
-
-            //Test if we can delete m4 who has an order
-            Admin.AddOrder(m4.MemberID, a1.ArticleID, 5);
-            Admin.DeleteMember(m4.MemberID);
-            Assert.IsNotNull(m4);
-            Admin.DeleteOrders();
-        }
-
-        [TestMethod]
-        public void DeleteMembers()
-        {
-            //Test if we can delete all members if someone currently are training
-            Admin.InsertTrainingMember(m4.MemberID);
-            Admin.DeleteMembers();
-            Assert.IsNotNull(Admin.ListMembers());
-            Admin.DeleteTrainingMember(m4.MemberID);
-
-            //Test if we can delete all members if someone has an order
-            Admin.AddOrder(m4.MemberID, a1.ArticleID, 5);
-            Admin.DeleteMembers();
-            Assert.IsNotNull(Admin.ListMembers());
-            Admin.DeleteOrders();
-
-            Admin.DeleteMembers();
-            Assert.IsNull(Admin.ListMembers());
+            //delete member m1
+            Admin.DeleteMember(m1.MemberID);
+            Assert.IsNull(Query.GetMemberDetails(m1.MemberID));
         }
     }
 }
